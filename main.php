1630b6390d952719e8db696bd2c249d7c0f526f1
Mario Flores <umlouco@gmail.com>
2025-10-17 18:23:57 +0000

    add plugin info

--- main.php
+++ main.php
@@ -82,40 +82,78 @@ function mfmanager_cron_exec()
     require_once ABSPATH . 'wp-admin/includes/plugin.php';
     require_once ABSPATH . 'wp-admin/includes/update.php';
     require_once ABSPATH . 'wp-admin/includes/file.php';
-    $theme = wp_get_theme(); 
+
+    wp_update_plugins();
+    wp_update_themes();
+
     $info = array();
     $info['name'] = get_bloginfo('name');
     $info['version']  = get_bloginfo('version');
-    $info['plugins'] = get_plugin_updates();
     $info['url'] = get_bloginfo('url');
     $info['template'] = get_bloginfo('template_url');
     $info['path'] = get_home_path();
-    $info['ip'] = $_SERVER['SERVER_ADDR'];
-    $info['key'] =  get_option('mfmanager_api_settings');
-    $info['theme_version'] = $theme->Version; 
-    $data_string = json_encode($info);
-    $curl = curl_init();
-    try {
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://office.mario-flores.com/api/updateSite',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $data_string,
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json'
-            ),
-        ));
+    $info['ip'] = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '';
+    $info['key'] = get_option('mfmanager_api_settings');
 
-        $response = curl_exec($curl);
+    $theme = wp_get_theme();
+    $theme_updates = get_site_transient('update_themes');
+    $theme_slug = $theme->get_stylesheet();
+    $theme_update = null;
 
-        echo $response;
-    } catch (\Exception $ex) {
-        echo $ex->getMessage();
+    if ($theme_updates && !empty($theme_updates->response) && isset($theme_updates->response[$theme_slug])) {
+        $theme_update = $theme_updates->response[$theme_slug];
     }
-    curl_close($curl);
+
+    $info['theme'] = array(
+        'name'             => $theme->get('Name'),
+        'version'          => $theme->get('Version'),
+        'stylesheet'       => $theme->get_stylesheet(),
+        'template'         => $theme->get_template(),
+        'update_available' => !empty($theme_update),
+        'new_version'      => $theme_update ? $theme_update['new_version'] : null,
+        'package'          => $theme_update ? $theme_update['package'] : null,
+    );
+
+    $plugins = get_plugins();
+    $plugin_updates = get_site_transient('update_plugins');
+    $plugins_info = array();
+
+    foreach ($plugins as $plugin_file => $plugin_data) {
+        $update = null;
+
+        if ($plugin_updates && isset($plugin_updates->response[$plugin_file])) {
+            $update = $plugin_updates->response[$plugin_file];
+        }
+
+        $plugins_info[] = array(
+            'name'             => $plugin_data['Name'],
+            'version'          => $plugin_data['Version'],
+            'plugin_file'      => $plugin_file,
+            'update_available' => !empty($update),
+            'new_version'      => $update ? $update->new_version : null,
+            'package'          => $update ? $update->package : null,
+        );
+    }
+
+    $info['plugins'] = $plugins_info;
+
+    $response = wp_remote_post(
+        'https://office.mario-flores.com/api/updateSite',
+        array(
+            'headers' => array(
+                'Content-Type' => 'application/json',
+            ),
+            'body'    => wp_json_encode($info),
+            'timeout' => 20,
+        )
+    );
+
+    if (is_wp_error($response)) {
+        error_log('MF Manager error: ' . $response->get_error_message());
+        return;
+    }
+
+    if (wp_remote_retrieve_response_code($response) >= 400) {
+        error_log('MF Manager error: ' . wp_remote_retrieve_body($response));
+    }
 }
